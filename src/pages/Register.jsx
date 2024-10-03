3977f67c8803632df6acf28a674124b2abde528a
selene_gabriela <sel_nn@hotmail.com>
2024-10-02 22:44:01 -0600

    validator email password and name

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const validate = (input) => {
     const errors = {}
-    const regexEmail = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
-    const regexPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$")
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const regexPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W]).{8,}$/;
+
     if(input.name.length < 4) errors.name = 'Name should be at least 3 characters long.';
     if(!regexEmail.test(input.email)) errors.email = 'Invalid email'
-    if(!regexPassword.test(input.password)) errors.email = 'The password must be at least 8 characters long, including a number, an uppercase letter, a lowercase letter, and a special character.'
+    if(!regexPassword.test(input.password)) errors.password = 'The password must be at least 8 characters long, including a number, an uppercase letter, a lowercase letter, and a special character.'
     return errors;
 }
 
 const Register = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
     const [errors, setErrors] = useState({})
     const [input,setInput] = useState({
         name: '',
@@ -19,24 +22,59 @@ const Register = () => {
         password: ''
     })
 
+    const handleOnChange = (e) => {
+        e.preventDefault();
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+        setErrors(validate({
+            ...input,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={e => handleSubmit(e)}>
                 <label>User name: </label>
-                <input name='name' value={input.name} type='text' placeholder='User name'/>
+                <input name='name' value={input.name} type='text' placeholder='User name' onChange={e => handleOnChange(e)}/>
                 {
-                    errors.name && <label>errors.name</label>
+                    errors.name && <label>{errors.name}</label>
                 }
                 <label>Email: </label>
-                <input name='email' value={input.email} type='email' placeholder='Email'/>
+                <input name='email' value={input.email} type='email' placeholder='Email' onChange={e => handleOnChange(e)}/>
                 {
-                    errors.email && <label>errors.email</label>
+                    errors.email && <label>{errors.email}</label>
                 }
                 <label>Password: </label>
-                <input name='password' value={input.password} type='password' placeholder='Password'/>
+                <div className="password-input">
+                    <input
+                        name="password"
+                        value={input.password}
+                        type={passwordVisible ? "text" : "password"}
+                        placeholder="Password"
+                        onChange={e => handleOnChange(e)}
+                        className="password-input__input"
+                    />
+                    <span 
+                        onMouseDown={() => setPasswordVisible(true)} 
+                        onMouseUp={() => setPasswordVisible(false)} 
+                        onMouseLeave={() => setPasswordVisible(false)} // Para manejar cuando el mouse sale del ícono
+                        className="password-input__icon"
+                    >
+                    <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+                    </span>
+                </div>
                 {
-                    errors.password && <label>errors.password</label>
+                    errors.password && <label>{errors.password}</label>
                 }
+                <button type="submit">Sign Up</button>
             </form>
         </div>
     )
